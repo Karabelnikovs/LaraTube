8b6b0eac0328fda9935eb41361d9c7f322a04f6f
Karabelnikovs <125544102+Karabelnikovs@users.noreply.github.com>
2024-05-03 13:35:28 +0300

    videos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/upload_v', [App\Http\Controllers\VideoController::class, 'index']);
+
+Route::post('/insert_video', [App\Http\Controllers\VideoController::class, 'insert'])->name('insert.file');
+
+Route::get('/fetch_video', [App\Http\Controllers\VideoController::class, 'fetch']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
